683372f1caf17967ca3e7c66df9f7d199459ccb0
djokjulapfe <djordjemarjanovic998@gmail.com>
2017-10-31 19:45:33 +0100

    rearanged commands so that visuals are set befor dependency download

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
-
 #set radiance (light) theme
 gsettings set org.gnome.desktop.interface gtk-theme 'Radiance'
-gsettings set org.gnome.desktop.interface icon-theme 'ubuntu-mono-dark'
+gsettings set org.gnome.desktop.interface icon-theme 'ubuntu-mono-light'
 
 #auto hide louncher
 dconf write "/org/compiz/profiles/unity/plugins/unityshell/launcher-hide-mode" 1
@@ -12,13 +11,19 @@ gsettings set org.gnome.settings-daemon.plugins.media-keys screensaver '<Super>l
 #create file system
 mkdir Pictures/Wallpapers Documents/work Documents/work/YDrive Gems Applications
 
+#set wallpaper
+cp ~/ubuntusetup/wallpaper.png ~/Pictures/Wallpapers/Ywallpaper.png
+gsettings set org.gnome.desktop.background picture-uri file:///home/djokjula/Pictures/Wallpapers/Ywallpaper.jpg
+
+#download dependecies
+sudo apt-get install build-essential terminator ssh \
+openssh-server sshfs python3 psensor htop unity-tweak-tool \
+texstudio vim octave gimp inkscape python3-pip \
+calibre geogebra wesnoth-1.12
+
 #install opencv
 pip install --upgrade pip
 pip3 install opencv-python
-
-#set wallpaper
-cp ~/ubuntusetup/wallpaper.png ~/Pictures/Wallpapers/Ywallpaper.png
-gsettings set org.gnome.desktop.background picture-uri file:///home/djokjula/Pictures/Wallpapers/Ywallpaper.png
 
 #install vs code
 wget -O Downloads/code.deb https://go.microsoft.com/fwlink/?LinkID=760868
@@ -29,7 +34,7 @@ wget -O Downloads/jetbrains-toolbox.tar.gz https://download.jetbrains.com/toolbo
 tar -zxvf Downloads/jetbrains-toolbox.tar.gz -C ~/Applications/
 Applications/jetbrains-toolbox-1.5.2871/jetbrains-toolbox
 
-#install meshlab & zotero
+#install meshlab, zotero & sublime
 sudo add-apt-repository ppa:zarquon42/meshlab
 sudo add-apt-repository ppa:smathot/cogscinl
 sudo add-apt-repository ppa:webupd8team/sublime-text-3
